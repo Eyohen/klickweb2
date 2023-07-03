3ddd8ea0a8c47d75d4a7a249d7ea515b41d9a874
Joshua <cutejosh2@gmail.com>
2023-07-03 21:44:11 +0100

    using yup and react hook form to validate create store input

--- src/scenes/CreateStore/index.jsx
+++ src/scenes/CreateStore/index.jsx
@@ -1,7 +1,29 @@
 import Logo from '../../assets/logo.png'
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const validationSchema = yup.object().shape({
+    name_of_store: yup.string().required('Name is required'),
+    email: yup.string().email('Invalid email').required('Email is required'),
+    phone: yup.string().required('Phone number is required'),
+    address: yup.string().required('Address is required').min(10, 'Address is too short'),
+});
 
 const CreateStore = () => {
-    const inputClasses = "border border-gray-200 bg-gray-50 outline-none rounded-md px-4 py-2 w-full text-gray-600 focus:ring-secondary focus:border-secondary"
+    const inputClasses =
+        'border bg-gray-50 outline-none rounded-md px-4 py-2 w-full text-gray-600 focus:ring-secondary focus:border-secondary';
+    const errorBorderClasses = 'border-red-500';
+
+    const { handleSubmit, register, formState: { errors } } = useForm({
+        resolver: yupResolver(validationSchema),
+    });
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+
     return (
         <div className="flex flex-col items-center w-[25%] mx-auto space-y-5 mt-20">
             <img src={Logo} alt="Logo" className="" />
@@ -10,23 +32,67 @@ const CreateStore = () => {
                 <p className='text-gray-500 text-sm font-semibold'>Let's Get to Know Your Business Better</p>
             </div>
 
-            <form className='flex flex-col w-full'>
-                <div className='mb-4'>
-                    <label className='block text-sm' htmlFor="name_of_store">Name of Store</label>
-                    <input type="text" name="name_of_store" id="name_of_store" className={inputClasses} />
+            <form className="flex flex-col w-full" onSubmit={handleSubmit(onSubmit)}>
+                <div className="mb-4">
+                    <label className="block text-sm" htmlFor="name_of_store">
+                        Name of Store
+                    </label>
+                    <input
+                        type="text"
+                        name="name_of_store"
+                        id="name_of_store"
+                        className={`${inputClasses} ${errors.name_of_store ? errorBorderClasses : ''}`}
+                        {...register('name_of_store')}
+                    />
+                    {errors.name_of_store && (
+                        <span className="text-red-500 text-xs">{errors.name_of_store.message}</span>
+                    )}
                 </div>
-                <div className='mb-4'>
-                    <label className='block text-sm' htmlFor="email">Email Address</label>
-                    <input type="email" name="email" id="email" className={inputClasses} />
+                {/* ... */}
+                <div className="mb-4">
+                    <label className="block text-sm" htmlFor="email">
+                        Email Address
+                    </label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className={`${inputClasses} ${errors.email ? errorBorderClasses : ''}`}
+                        {...register('email')}
+                    />
+                    {errors.email && <span className="text-red-500 text-xs">{errors.email.message}</span>}
                 </div>
-                <div className='mb-4'>
-                    <label className='block text-sm' htmlFor="phone">Business Phone Number</label>
-                    <input type="number" name="phone" id="phone" className={inputClasses} />
+                {/* ... */}
+                <div className="mb-4">
+                    <label className="block text-sm" htmlFor="phone">
+                        Business Phone Number
+                    </label>
+                    <input
+                        type="number"
+                        name="phone"
+                        id="phone"
+                        className={`${inputClasses} ${errors.phone ? errorBorderClasses : ''}`}
+                        {...register('phone')}
+                    />
+                    {errors.phone && <span className="text-red-500 text-xs">{errors.phone.message}</span>}
                 </div>
-                <div className=''>
-                    <label className='block text-sm' htmlFor="address">Enter Delivery Address</label>
-                    <input type="text" name="address" id="address" className={inputClasses} />
+                {/* ... */}
+                <div className="">
+                    <label className="block text-sm" htmlFor="address">
+                        Enter Delivery Address
+                    </label>
+                    <input
+                        type="text"
+                        name="address"
+                        id="address"
+                        className={`${inputClasses} ${errors.address ? errorBorderClasses : ''}`}
+                        {...register('address')}
+                    />
+                    {errors.address && (
+                        <span className="text-red-500 text-xs">{errors.address.message}</span>
+                    )}
                 </div>
+                {/* ... */}
                 <button className='bg-gray-50 py-4 text-gray-500 rounded-full mt-10' type='submit'>
                     Continue
                 </button>
